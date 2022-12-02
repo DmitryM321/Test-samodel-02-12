f06ee8f63e63626343656bfb9f09b909b0419040
Dmitry <a89297175356@gmail.com>
2022-12-02 11:23:28 +0500

    Лови!!

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("10:58 Fist chached");
+        System.out.println("");
+        int a = 34;
+        System.out.println(a);
+    }
+}
+
+
